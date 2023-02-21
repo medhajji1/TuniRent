544f3dc302934d062c58e347b056b06fc2e8c082
tchob1 <karim.chabchoub@esprit.tn>
2023-02-21 12:45:35 +0100

    karim chabchoub

---
+++ GestionUsers/src/Services/IService.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Services;
+
+import java.util.List;
+
+/**
+ *
+ * @author abdelazizmezri
+ */
+public interface IService<T> {
+    public void ajouter(T u);
+    public void supprimer(int CIN);
+    public boolean modifier(T u);
+    public List<T> getAll();
+}
+
